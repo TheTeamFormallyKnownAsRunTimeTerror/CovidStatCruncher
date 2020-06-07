354c05306838e72dd270c480031665172b984f8b
Bernard Laird <bernard.laird@paddypowerbetfair.com>
2020-06-07 19:34:33 +0100

    Add skeleton Application.
    Add docker.
    Add Swagger

---
+++ src/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CovidStatCruncher
+{
+    public static class Constants
+    {
+        public static string ApplicationName => "CovidStatCruncher";
+    }
+}
---
+++ src/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CovidStatCruncher.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
